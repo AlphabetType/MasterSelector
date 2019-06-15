dc8809b73cafd0603bf163300fbd8ee0209a7e6f
Marcin Dybaś <marcin.dybas@me.com>
2019-06-15 04:38:02 +0200

    Synced slider with the list, added additional error windows, updated Info.plist

--- MasterSelector.glyphsPlugin/Contents/Resources/plugin.py
+++ MasterSelector.glyphsPlugin/Contents/Resources/plugin.py
@@ -36,14 +36,16 @@ class MasterSelectorWindow(GeneralPlugin):
 	
 	def showWindow(self, sender):
 		if Glyphs.font is None:
-			print "Master Selector: Please open a font first"
+			self.helperWindow("Please open a font first.")
+		elif len(Glyphs.font.masters) < 2:
+			self.helperWindow("Your font has only 1 master.")
 		else:
 			mastersList = []
 			for m in Glyphs.font.masters:
 				mastersList.append(m.name)
 			currentMasterIndex = Glyphs.font.masterIndex
 
-			self.windowWidth = 200
+			self.windowWidth = 250
 			self.windowHeight = 25*len(mastersList)+23+30
 			self.w = FloatingWindow((self.windowWidth, self.windowHeight), "Master Selector")
 			self.w.radiomasters = RadioGroup((10, 10, -10, 25*len(mastersList)),
@@ -56,22 +58,34 @@ class MasterSelectorWindow(GeneralPlugin):
 	                            minValue = 0,
 	                            maxValue = len(mastersList)-1,
 	                            sizeStyle = "small",
+								continuous= False,
 	                            callback=self.changeMasterSlider)
 			
 			self.w.open()
+
+	def helperWindow(self, message):
+		self.w = FloatingWindow((200, 80), "Master Selector")
+		self.w.error = TextBox((10,10,-10,20), message, alignment="center")
+		self.w.button = Button((10,40,-10,20), "Close", self.closeWindow)
+		self.w.open()
+
+	def closeWindow(self,sender):
+		self.w.close()
 
 	def changeMaster(self, sender):
 		currentChoice = self.w.radiomasters.get()
 		try:
 			Glyphs.font.parent.windowController().setMasterIndex_(currentChoice)
+			self.w.slider.set(currentChoice)
 		except:
 			print 'BROKEN'
 			pass
 
 	def changeMasterSlider(self, sender):
-		currentChoice = self.w.slider.get()
+		currentChoice = int(self.w.slider.get())
 		try:
 			Glyphs.font.parent.windowController().setMasterIndex_(currentChoice)
+			self.w.radiomasters.set(currentChoice)
 		except:
 			print 'BROKEN'
 			pass
