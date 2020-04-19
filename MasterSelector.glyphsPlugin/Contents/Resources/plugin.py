a22a54300fe2be42afbfff1ce5e9e44aefd7b034
Rainer Scheichelbauer <res@glyphsapp.com>
2020-04-19 17:03:51 +0200

    more efficent imports, localisation

--- MasterSelector.glyphsPlugin/Contents/Resources/plugin.py
+++ MasterSelector.glyphsPlugin/Contents/Resources/plugin.py
@@ -1,28 +1,40 @@
 # encoding: utf-8
 from __future__ import division, print_function, unicode_literals
 
-###########################################################################################################
+##############################################################################
 #
 #
-#	Master Selector Plugin
+#  Master Selector Plugin
 #
-#	With this plugin you can navigate multi-master Glyphs Files more easily.
-#	Slider included.
+#  With this plugin you can navigate multi-master Glyphs Files more easily.
+#  Slider included.
 #
-#	Benedikt Bramboeck, 2018
+#  Benedikt Bramboeck, 2018
 #
-###########################################################################################################
+##############################################################################
 
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
-from vanilla import *
+from vanilla import Slider, FloatingWindow, RadioGroup
 
 class MasterSelectorWindow(GeneralPlugin):
 	
 	@objc.python_method
 	def settings(self):
 		self.name = 'Master Selector'
+		self.warningOnlyOneMaster = Glyphs.localize({
+			"en": "Your font has only one master.",
+			"de": "Die Schrift hat nur einen Master.",
+			"fr": "La police contient seulement un master.",
+			"es": "La fuente tiene solamente un máster.",
+		})
+		self.warningNoFontOpen = Glyphs.localize({
+			"en": "Please open a font first.",
+			"de": "Bitte öffnen Sie erst eine Schrift.",
+			"fr": "Veuillez ouvrir un fichier de police.",
+			"es": "Por favor, primero abrir una fuente.",
+		})
 	
 	@objc.python_method
 	def start(self):
@@ -39,9 +51,9 @@ class MasterSelectorWindow(GeneralPlugin):
 	
 	def showWindow_(self, sender):
 		if Glyphs.font is None:
-			self.helperWindow("Please open a font first.")
+			self.helperWindow(self.warningNoFontOpen)
 		elif len(Glyphs.font.masters) < 2:
-			self.helperWindow("Your font has only 1 master.")
+			self.helperWindow(self.warningOnlyOneMaster)
 		else:
 			mastersList = []
 			for m in Glyphs.font.masters:
@@ -50,29 +62,25 @@ class MasterSelectorWindow(GeneralPlugin):
 
 			self.windowWidth = 250
 			self.windowHeight = 25*len(mastersList)+23+30
-			self.w = FloatingWindow((self.windowWidth, self.windowHeight), "Master Selector")
+			self.w = FloatingWindow((self.windowWidth, self.windowHeight), self.name)
 			self.w.radiomasters = RadioGroup((10, 10, -10, 25*len(mastersList)),
-	                                mastersList,
-	                                callback=self.changeMaster)
+				mastersList,
+				callback=self.changeMaster)
 			self.w.slider = Slider((10, -35, -10, 23),
-	                            tickMarkCount=len(mastersList),
-	                            stopOnTickMarks=True,
-	                            value=currentMasterIndex,
-	                            minValue=0,
-	                            maxValue=len(mastersList)-1,
-	                            sizeStyle="small",
-				    continuous=False,
-	                            callback=self.changeMasterSlider)
+				tickMarkCount=len(mastersList),
+				stopOnTickMarks=True,
+				value=currentMasterIndex,
+				minValue=0,
+				maxValue=len(mastersList)-1,
+				sizeStyle="small",
+				continuous=False,
+				callback=self.changeMasterSlider)
 			
 			self.w.open()
 
 	@objc.python_method
 	def helperWindow(self, message):
-		Message(
-			title="Master Selector",
-			message=message,
-			OKButton=None,
-			)
+		Message(title=self.name, message=message, OKButton=None)
 
 	@objc.python_method
 	def changeMaster(self, sender):
